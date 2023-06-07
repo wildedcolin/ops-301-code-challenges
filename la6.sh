8f8cb1c8e4ba30e8a7e677a1aab3bb8031067283
wildedcolin <130380754+wildedcolin@users.noreply.github.com>
2023-06-07 11:19:46 -0700

    Update la6.sh
    this is the edit

--- la6.sh
+++ la6.sh
@@ -1,1 +1,16 @@
+import os
 
+# Declare and initialize variables
+
+# you can use os. system to execute the Bash command
+os.system("ls")
+
+# Assigned Variables
+ip = os.system ("ip -c a")
+user= os.system("whoami")
+specs = os.system("lshw -short")
+
+#Printing listed variables
+print(ip)
+print(user)
+print(specs)
