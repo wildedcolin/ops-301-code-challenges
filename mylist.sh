ef3ce01010db2f0e9d8a52132fcadfdef601e7c0
wildedcolin <130380754+wildedcolin@users.noreply.github.com>
2023-06-08 23:06:23 -0700

    Create mylist.sh

---
+++ mylist.sh
@@ -0,0 +1,13 @@
+my_list = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "indigo", "jackfruit"]
+
+# Print the fourth element of the list
+print("Fourth element:", my_list[3])
+
+# Print the sixth through tenth element of the list
+print("Sixth to tenth elements:", my_list[5:10])
+
+# Change the value of the seventh element to "onion"
+my_list[6] = "onion"
+
+# Print the modified list
+print("Modified list:", my_list)
