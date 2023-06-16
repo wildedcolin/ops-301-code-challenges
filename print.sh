713f47d7ecf80a567ae012138d80d1a30ee238be
wildedcolin <130380754+wildedcolin@users.noreply.github.com>
2023-06-16 15:11:35 -0700

    Create print.sh

---
+++ print.sh
@@ -0,0 +1,39 @@
+# Example values for variables a and b
+a = 5
+b = 10
+
+# Equals: a == b
+if a == b:
+    print("a is equal to b")
+else:
+    print("a is not equal to b")
+
+# Not Equals: a != b
+if a != b:
+    print("a is not equal to b")
+else:
+    print("a is equal to b")
+
+# Less than: a < b
+if a < b:
+    print("a is less than b")
+else:
+    print("a is not less than b")
+
+# Less than or equal to: a <= b
+if a <= b:
+    print("a is less than or equal to b")
+else:
+    print("a is greater than b")
+
+# Greater than: a > b
+if a > b:
+    print("a is greater than b")
+else:
+    print("a is not greater than b")
+
+# Greater than or equal to: a >= b
+if a >= b:
+    print("a is greater than or equal to b")
+else:
+    print("a is less than b")
